add638afc5703b1c5f99ff80b9277f69bc762960
Sarath Raj <sarathrj10@gmail.com>
2020-08-04 17:48:09 +0530

    q2 compleated

---
+++ q2.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+ let num1 = parseInt(prompt("Enter number 1 :"));
+ let num2 = parseFloat(prompt("Enter number 2 :"));
+ let sum = num1 + num2;
+ console.log(`sum :${sum}`);
